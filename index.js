4cd8a1ee12e3519d5c56b54e456ae6464a7d7ec5
edge <dev@ionized.io>
2016-04-03 00:01:28 -0400

    add default config

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ export let plain = (relativePath) =>
 		require(absolute ? path : join(relativePath, path));
 
 export let babel = (relativePath, babelOptions) => {
-	require('babel-register')(babelOptions);
+	require('babel-register')({
+		ast: false,
+		comments: false,
+		compact: true,
+		...babelOptions
+	});
 	return plain(relativePath);
 };
